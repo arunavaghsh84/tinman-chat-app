1bffa99fd9f7a3af64e4db9b6acfb706ffce3234
Arunava Ghosh <arunavaghsh84@gmail.com>
2024-08-19 13:54:27 +0530

    Add comment and doc string to the code

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -4,8 +4,8 @@ const connectMongo = async () => {
     if (mongoose.connections[0].readyState) return;
 
     await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        useNewUrlParser: true, // Allows to use the old connection string parser if they find a bug in the new parser
+        useUnifiedTopology: true, // Allows to use a new connection management engine for the MongoDB driver
     });
 };
 
